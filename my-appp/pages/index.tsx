962f0b32e358546a24ec6587106af21a9622fe52
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-01 08:49:31 +0100

    index

--- my-appp/pages/index.tsx
+++ my-appp/pages/index.tsx
@@ -102,4 +102,14 @@ export default function Home() {
 }
 };
 
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
 
