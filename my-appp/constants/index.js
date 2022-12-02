ee6101e0e2013c3a933b85766fd8b873ae5ac524
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-02 10:08:32 +0100

    index

--- my-appp/constants/index.js
+++ my-appp/constants/index.js
@@ -1,1 +1,13 @@
 export const WHITELIST_CONTRACT_ADDRESS = "0x67581Eb0772D683fa1d10811D49bF59E21041f01";
+export const abi = [
+    {
+        "inputs": [
+          {
+            "internalType": "uint8",
+            "name": "_maxWhitelistedAddresses",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
